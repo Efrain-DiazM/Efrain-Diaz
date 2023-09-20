ff03cac5b5bb0ad768572ac1dfddc35930fb8cdb
EfrainDiazM <efradiaz28@gmail.com>
2023-09-19 22:02:36 -0500

    fonts-colors

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        HOLA Efrain!
+    <section className="w-screen h-screen">
+      <h1 className="font-roboto text-xl align-bottom text-bottle-green-600 text-center">
+        Hola soy Efrain Diaz!
       </h1>
+    </section>
+      
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'Anton']
+      },
+      colors: {
+        'bottle-green': {
+          '50': '#f1fcfa',
+          '100': '#cef9ee',
+          '200': '#9ef1de',
+          '300': '#65e3ca',
+          '400': '#36cbb3',
+          '500': '#1cb099',
+          '600': '#148d7d',
+          '700': '#147166',
+          '800': '#155a53',
+          '900': '#164b45',
+          '950': '#052422',
+      },      
+      }
+    },
   },
   plugins: [],
 }
